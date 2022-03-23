04f5bc49545ae8c4ce17a2fe3392b9917134248b
Brandon <buibrandon47@gmail.com>
2022-03-23 10:43:58 +0100

    Creation d'un nouveau projet

---
+++ src/main/java/module-info.java
@@ -0,0 +1,10 @@
+module com.example.classemodele {
+    requires javafx.controls;
+    requires javafx.fxml;
+
+    requires org.controlsfx.controls;
+
+    opens com.example.classemodele to javafx.fxml;
+    exports com.example.classemodele;
+    exports vue;
+}
