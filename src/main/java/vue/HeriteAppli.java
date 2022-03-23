c3235f7bbc521a6536efbdbf24ce49e8a0431b46
Brandon <buibrandon47@gmail.com>
2022-03-23 11:46:00 +0100

    Mise en place des classe

--- src/main/java/vue/HeriteAppli.java
+++ src/main/java/vue/HeriteAppli.java
@@ -1,9 +1,21 @@
 package vue;
 
 import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
 
 public class HeriteAppli extends Application {
+    public void start(Stage stage) {
+        VBoxRoot root = new VBoxRoot();
+
+        Scene scene = new Scene(root, 300, 80);
+        stage.setScene(scene);
+        stage.setTitle("Hello JavaFX");
+        stage.show();
+    }
+
     public static void main(String[] args) {
+        Application.launch(args);
 
     }
 }
--- src/main/java/vue/VBoxRoot.java
+++ src/main/java/vue/VBoxRoot.java
@@ -1,5 +1,17 @@
 package vue;
 
-public class VBoxRoot {
+import javafx.scene.control.Label;
+import javafx.scene.layout.VBox;
+import modele.Date;
+
+public class VBoxRoot extends VBox {
+    public VBoxRoot(){
+
+        Date DateD = new Date();
+        Label labelHello = new Label(DateD.toString());
+        this.getChildren().add(labelHello);
+    }
+
+
 
 }
