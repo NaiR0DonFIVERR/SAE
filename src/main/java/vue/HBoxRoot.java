4296e48b4e4f0469cb5be6309be9fa8753b27b49
suki <buibrandon47@gmail.com>
2022-05-18 20:11:05 +0200

    intitialisation des geteurs pour le controleur

--- src/main/java/vue/HBoxRoot.java
+++ src/main/java/vue/HBoxRoot.java
@@ -1,32 +1,51 @@
 package vue;
 
+import ComposantGraphique.Box.BoxScenario;
+import ComposantGraphique.Box.MenuBarChoixUtilisateur;
+import ComposantGraphique.Scroll.ScrollPaneVille;
 import ComposantGraphique.Scroll.SrollPaneCli;
-import javafx.scene.control.Button;
-import javafx.scene.control.Label;
-import javafx.scene.control.ScrollPane;
 import javafx.scene.layout.HBox;
-import javafx.scene.layout.VBox;
 import modele.*;
 
 
 import java.io.File;
 import java.io.IOException;
-import java.util.*;
 
-import static modele.LectureEcriture.recupVille;
 import static modele.LectureEcriture.lectureScenario;
-import static modele.VoisinEntrants.*;
 
 public class HBoxRoot extends HBox {
     File fichier = new File("E:\\java\\SAé\\ClasseModele\\src\\main\\java\\fichier_txt\\scenario_0.txt");
     Scenario scenario = lectureScenario(fichier);
 
+    static ScrollPaneVille scrollPaneVille = new ScrollPaneVille();
+    static SrollPaneCli srollPaneCli = new SrollPaneCli();
+    static BoxScenario boxScenario = new BoxScenario();
+
+    MenuBarChoixUtilisateur boxMenu = new MenuBarChoixUtilisateur();
 
     public HBoxRoot() throws IOException {
-        SrollPaneCli srollPaneCli = new SrollPaneCli(scenario);
+
+
+        boxScenario.addScenario(scenario);
+
+        scrollPaneVille.setScollVille(scenario);
+        srollPaneCli.setScoll(scenario);
+
+        this.getChildren().add(boxMenu.getMenuBar());
+        this.getChildren().add(boxScenario.getBox());
         this.getChildren().add(srollPaneCli.getScrollPane());
+        this.getChildren().add(scrollPaneVille.getScrollPane());
     }
 
+    public static ScrollPaneVille getScrollPaneVille(){
+        return scrollPaneVille;
+    }
+    public static SrollPaneCli getSrollPaneCli(){
+        return srollPaneCli;
+    }
+    public BoxScenario getBoxScenario(){
+        return boxScenario;
+    }
 
 
 }
