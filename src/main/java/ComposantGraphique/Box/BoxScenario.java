fb7a612cd833c589b4f939885d64b7e496df2ec7
suki <buibrandon47@gmail.com>
2022-05-18 20:07:15 +0200

    ajout d'une box : BoxScenario

---
+++ src/main/java/ComposantGraphique/Box/BoxScenario.java
@@ -0,0 +1,44 @@
+package ComposantGraphique.Box;
+
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.ScrollPane;
+import javafx.scene.layout.VBox;
+import modele.Scenario;
+
+import java.util.ArrayList;
+import java.util.List;
+
+
+public class BoxScenario extends VBox {
+
+    VBox box;
+    VBox boxScenario;
+    ScrollPane scrollPane;
+    List<Scenario> scenarioList;
+
+    public BoxScenario(){
+        box = new VBox();
+        boxScenario = new VBox();
+        scrollPane = new ScrollPane();
+        scenarioList = new ArrayList<>();
+        scrollPane.setContent(boxScenario);
+        box.getChildren().addAll(new Label("Liste des Scenarios"), scrollPane);
+    }
+
+    public void addScenario(Scenario scenario){
+        Button button = new Button("Scenario : " + scenarioList.size());
+        scenarioList.add(scenario);
+        button.setUserData(scenario);
+        boxScenario.getChildren().add(button);
+    }
+
+    public ScrollPane getScrollPane(){
+        return scrollPane;
+    }
+
+    public VBox getBox(){
+        return box;
+    }
+
+}
