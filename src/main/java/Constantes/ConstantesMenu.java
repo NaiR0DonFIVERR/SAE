d52b38e65158fe396e9543aac2846f932c647306
suki <buibrandon47@gmail.com>
2022-05-23 22:52:44 +0200

    ajout d'une nouvelle constances CHOIX_CHEMIN

--- src/main/java/Constantes/ConstantesMenu.java
+++ src/main/java/Constantes/ConstantesMenu.java
@@ -3,4 +3,5 @@ package Constantes;
 public interface ConstantesMenu {
 
     final String [] CHOIX_MENU = {"Ajout Scenario", "Supprimer Scenario", "Liste des Chemins"};
+    final String [] CHOIX_CHEMIN = {"Premier Chemin", "Trois premiers Chemin", "Tout les chemins"};
 }
