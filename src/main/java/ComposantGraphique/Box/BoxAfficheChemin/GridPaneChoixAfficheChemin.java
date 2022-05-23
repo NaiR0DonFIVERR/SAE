134f945557682c038e3c53b01ef44223f273da6a
suki <buibrandon47@gmail.com>
2022-05-23 23:05:53 +0200

    ajout des bouttons dans le GridPaneChoixAfficheChemin

--- src/main/java/ComposantGraphique/Box/BoxAfficheChemin/GridPaneChoixAfficheChemin.java
+++ src/main/java/ComposantGraphique/Box/BoxAfficheChemin/GridPaneChoixAfficheChemin.java
@@ -1,6 +1,8 @@
 package ComposantGraphique.Box.BoxAfficheChemin;
 
-import javafx.scene.control.ToggleButton;
+import javafx.geometry.Insets;
+import javafx.scene.control.Label;
+import javafx.scene.control.RadioButton;
 import javafx.scene.control.ToggleGroup;
 import javafx.scene.layout.GridPane;
 
@@ -15,8 +17,23 @@ public class GridPaneChoixAfficheChemin extends GridPane {
         gridPane = new GridPane();
         toggleGroup = new ToggleGroup();
 
+        gridPane.setGridLinesVisible(true);
+        gridPane.setPadding(new Insets(40));
+        gridPane.setHgap(20);
+        gridPane.setVgap(20);
 
+        gridPane.add(new Label("CHOIS"), 1, 0, 2,1);
 
+        for (int i=0;i<CHOIX_CHEMIN.length;i++){
+            RadioButton button = new RadioButton(CHOIX_CHEMIN[i]);
+            button.setUserData(i);
+            button.setToggleGroup(toggleGroup);
+            gridPane.add(button, i,1+i%2,1,1);
+        }
+    }
+
+    public GridPane getGridPane(){
+        return gridPane;
     }
 
 }
