df65d249115c5803d57461102d934ee92cec4234
suki <buibrandon47@gmail.com>
2022-05-24 22:06:22 +0200

    petite modification du constructeur : trois box sont maintenant instancié

--- src/main/java/ComposantGraphique/Box/BoxAfficheChemin/VBoxTroisChemins.java
+++ src/main/java/ComposantGraphique/Box/BoxAfficheChemin/VBoxTroisChemins.java
@@ -9,17 +9,21 @@ public class VBoxTroisChemins extends VBox{
 
     VBox boxAll;
     VBox boxOne;
+    VBox boxTree;
     HBox boxTwo;
 
     public VBoxTroisChemins(){
         boxOne = new VBox();
         boxAll = new VBox();
         boxTwo = new HBox();
-        boxAll.getChildren().addAll(boxOne, boxTwo);
+        boxTree = new VBox();
+        boxAll.getChildren().addAll(boxOne, boxTwo, boxTree);
     }
 
     public void setBoxAll(){
         boxOne.getChildren().add(gethBoxList().get(0).getChildren().get(0));
+        boxTwo.getChildren().add(gethBoxList().get(0).getChildren().get(1));
+        boxTree.getChildren().add(gethBoxList().get(0).getChildren().get(2));
 
 
     }
