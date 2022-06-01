12972d488275e44ecb8f8b990d28dae0a8aa891d
suki <buibrandon47@gmail.com>
2022-06-01 21:02:49 +0200

    changement de l'ordre dans la liste pour résoudre le problème

--- src/main/java/Constantes/ConstantesMenu.java
+++ src/main/java/Constantes/ConstantesMenu.java
@@ -2,6 +2,6 @@ package Constantes;
 
 public interface ConstantesMenu {
 
-    final String [] CHOIX_MENU = {"Ajout Scenario", "Supprimer Scenario", "Liste des Chemins"};
+    final String [] CHOIX_MENU = {"Liste des Chemins","Ajout Scenario", "Supprimer Scenario"};
     final String [] CHOIX_CHEMIN = {"Premier Chemin", "Trois premiers Chemin", "Tout les chemins"};
 }
