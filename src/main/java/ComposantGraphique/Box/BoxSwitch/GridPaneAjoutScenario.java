4e90d57cb03d660ee90a6019e5c635c8ecd4a7c4
suki <buibrandon47@gmail.com>
2022-06-01 10:33:35 +0200

    ajout de la classe GridPaneAjoutScenario : l'utilisateur pourra ajouter un scenario

---
+++ src/main/java/ComposantGraphique/Box/BoxSwitch/GridPaneAjoutScenario.java
@@ -0,0 +1,13 @@
+package ComposantGraphique.Box.BoxSwitch;
+
+import javafx.scene.layout.GridPane;
+
+public class GridPaneAjoutScenario extends GridPane {
+
+    GridPane gridPane;
+
+    public GridPaneAjoutScenario(){
+
+    }
+
+}
