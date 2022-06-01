0efd8d06d1980287a2307e270b1f872f0b00c9eb
suki <buibrandon47@gmail.com>
2022-06-01 10:56:43 +0200

    modification du constructeur : on initialise simplement le gridPane, on devra appeler la methode setGridPane pour ajouter les objets graphique dans le gridPane

--- src/main/java/ComposantGraphique/Box/BoxSwitch/GridPaneAjoutScenario.java
+++ src/main/java/ComposantGraphique/Box/BoxSwitch/GridPaneAjoutScenario.java
@@ -2,7 +2,12 @@ package ComposantGraphique.Box.BoxSwitch;
 
 import javafx.geometry.Insets;
 import javafx.scene.control.ComboBox;
+import javafx.scene.control.Label;
 import javafx.scene.layout.GridPane;
+
+import java.io.IOException;
+
+import static modele.LectureEcriture.getMembres;
 
 public class GridPaneAjoutScenario extends GridPane {
 
@@ -10,15 +15,22 @@ public class GridPaneAjoutScenario extends GridPane {
     ComboBox comboBoxVendeur;
     ComboBox comboBoxAcheteur;
 
-
     public GridPaneAjoutScenario(){
+        gridPane = new GridPane();
+    }
+
+    public void setGridPane() throws IOException {
 
         gridPane = new GridPane();
         gridPane.setPadding(new Insets(40));
         gridPane.setHgap(20);
         gridPane.setVgap(20);
         gridPane.setGridLinesVisible(true);
-
+        Label labelTitle = new Label("Ajout d'un scenario");
+        for (String cli : getMembres()){    //la methode retourne une liste de tout les clients;
+            comboBoxAcheteur.getItems().add(cli);
+            comboBoxVendeur.getItems().add(cli);
+        }
     }
 
     public GridPane getGridPane(){
