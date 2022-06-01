7b8707217c518211db82ee52988a3844d41cb430
suki <buibrandon47@gmail.com>
2022-06-01 10:48:46 +0200

    ajout de la methode getGridPane dans GridPaneAjoutScenario

--- src/main/java/ComposantGraphique/Box/BoxSwitch/GridPaneAjoutScenario.java
+++ src/main/java/ComposantGraphique/Box/BoxSwitch/GridPaneAjoutScenario.java
@@ -1,13 +1,28 @@
 package ComposantGraphique.Box.BoxSwitch;
 
+import javafx.geometry.Insets;
+import javafx.scene.control.ComboBox;
 import javafx.scene.layout.GridPane;
 
 public class GridPaneAjoutScenario extends GridPane {
 
     GridPane gridPane;
+    ComboBox comboBoxVendeur;
+    ComboBox comboBoxAcheteur;
+
 
     public GridPaneAjoutScenario(){
 
+        gridPane = new GridPane();
+        gridPane.setPadding(new Insets(40));
+        gridPane.setHgap(20);
+        gridPane.setVgap(20);
+        gridPane.setGridLinesVisible(true);
+
+    }
+
+    public GridPane getGridPane(){
+        return gridPane;
     }
 
 }
