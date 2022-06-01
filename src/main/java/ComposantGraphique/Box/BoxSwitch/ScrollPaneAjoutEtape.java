5243568782b3ec628acf6d64c141bcdf7ed9ff4d
suki <buibrandon47@gmail.com>
2022-06-01 21:38:13 +0200

    initialisation du constructeur :  -scrollpane  -vbox  -deux listes : vendeurs et acheteur methode ajout etape : à chaque appel ajoute un label qui correspond à l'acheteur et le vendeur ; ajout aussi ses valeurs dans les listes methode delete : remove tout les label de la box et les valeurs des listes

--- src/main/java/ComposantGraphique/Box/BoxSwitch/ScrollPaneAjoutEtape.java
+++ src/main/java/ComposantGraphique/Box/BoxSwitch/ScrollPaneAjoutEtape.java
@@ -1,7 +1,40 @@
 package ComposantGraphique.Box.BoxSwitch;
 
-public class ScrollPaneAjoutEtape {
+import javafx.scene.control.Label;
+import javafx.scene.control.ScrollPane;
+import javafx.scene.layout.VBox;
 
+import java.util.ArrayList;
+import java.util.List;
 
+public class ScrollPaneAjoutEtape extends ScrollPane {
+
+    ScrollPane scrollPane;
+    VBox box;
+    List<String> listVend;
+    List<String> listAch;
+
+    public ScrollPaneAjoutEtape(){
+        scrollPane = new ScrollPane();
+        box = new VBox();
+        listVend = new ArrayList<>();
+        listAch = new ArrayList<>();
+        scrollPane.setContent(box);
+    }
+
+    public void ajoutEtape(String acheteur, String vendeur){
+        box.getChildren().add(new Label(vendeur + " --> " + acheteur));
+        listVend.add(vendeur);
+        listAch.add(acheteur);
+    }
+
+    public void delete(){
+        int TAILLE = box.getChildren().size();
+        for (int i=0;i<TAILLE;i++){
+            box.getChildren().remove(0);
+            listVend.remove(0);
+            listAch.remove(0);
+        }
+    }
 
 }
