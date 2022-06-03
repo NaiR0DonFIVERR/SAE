d36fa37c50f1aa058202db67e901f5cb582a1384
suki <buibrandon47@gmail.com>
2022-06-03 09:53:21 +0200

    intitialisation du constructeur boxScenario

--- src/main/java/ComposantGraphique/Box/BoxSwitch/VBoxSupprimerScenario.java
+++ src/main/java/ComposantGraphique/Box/BoxSwitch/VBoxSupprimerScenario.java
@@ -1,9 +1,39 @@
 package ComposantGraphique.Box.BoxSwitch;
 
+import javafx.collections.ObservableList;
+import javafx.scene.Node;
+import javafx.scene.control.Button;
+import javafx.scene.control.ComboBox;
+import javafx.scene.control.Label;
 import javafx.scene.layout.VBox;
+import vue.VBoxRoot;
+
+
 
 public class VBoxSupprimerScenario extends VBox {
 
+    ComboBox comboBox;
 
+    public VBoxSupprimerScenario(){
+
+        comboBox = new ComboBox<>();
+        Label labelTitle = new Label("Choix du scenario a supprimer");
+        Button buttonSupprimer = new Button("Supprimer");
+        labelTitle.setId("lableTitle");
+        buttonSupprimer.setOnAction(VBoxRoot.getControler());
+        buttonSupprimer.setAccessibleText("SupprimerScenario");
+        ObservableList<Node> list = (VBoxRoot.getBoxScenario().getBoxScenario().getChildren());
+        for (int i=0;i<list.size();i++){
+            Button button = (Button) list.get(i);
+            String NOM = button.getText();
+            System.out.println(NOM);
+            comboBox.getItems().add(NOM);
+        }
+
+        this.getChildren().add(labelTitle);
+        this.getChildren().add(comboBox);
+        this.getChildren().add(buttonSupprimer);
+
+    }
 
 }
